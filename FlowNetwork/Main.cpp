3cb716be124921dd195d869f91d0febdcd303d03
unknown <royi44@gmail.com>
2020-08-12 20:01:58 +0300

    First commit

---
+++ FlowNetwork/Main.cpp
@@ -0,0 +1,43 @@
+/*
+Algorithm Course-Final Project
+-------------------------------
+
+The project purpose is to compare Ford-Fulkerson method by two different implements to find paths from source vertex
+to destination vertex in Flow Network: BFS method, Greedy method.
+Operation:
+The main function gets a text file name that includes the following parameters:
+<num of vertexes>
+<num of edges>
+<source vertex>
+<destination vertex>
+<trio of: v-u-capacity>
+.
+.
+.
+<trio of: v-u-capacity>
+#num of edges trios.
+
+The output is:
+BFS Method:
+Max flow
+Min cut
+Number of iterations
+Greedy Method:
+Max flow
+Min Cut
+Number of iterations
+*/
+
+
+#include "FordFulkerson.h"
+#include "Graph.h"
+
+int main(int argc, char* argv[])
+{
+	int sourceVert, destVert;
+	Graph& flowNetwork = Graph::GetGraphFromFile(argv[1], sourceVert, destVert);
+	FordFulkerson::BFS(flowNetwork, sourceVert, destVert);
+	flowNetwork.InitFlow();
+	FordFulkerson::Greedy(flowNetwork, sourceVert, destVert);
+	delete &flowNetwork;
+}
